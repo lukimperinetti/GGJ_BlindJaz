2d2c52b54d8e2471588db15818def9423fd737a5
BlindCapt <matthieujulie07@gmail.com>
2024-01-27 14:00:15 +0100

    Add day night cycle

---
+++ Assets/DayNight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+
+    Light sun;
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sun = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sun.transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
