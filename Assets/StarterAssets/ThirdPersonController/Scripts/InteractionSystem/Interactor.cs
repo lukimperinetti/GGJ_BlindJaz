ec271a70445fd4bac81cdf1846d9a137ee7f1869
lukimperinetti <luk.imperinetti@epitech.eu>
2024-01-27 03:42:23 +0100

    Prompt UI
    Have an issue : the prompt not displaying

--- Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Interactor.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Interactor.cs
@@ -11,20 +11,30 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private InteractionPromptUI _interactionPromptUI;
 
     private readonly Collider[] _colliders = new Collider[3];
-    [SerializeField] private int _numFund;
+    [SerializeField] private int _numFound;
+
+    private IInteractable _interactable;
 
     private void Update()
     {
-        _numFund = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
-        if (_numFund > 0)
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+        if (_numFound > 0)
         {
-            var interactable = _colliders[0].GetComponent<IInteractable>();
-            if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame)
+            _interactable = _colliders[0].GetComponent<IInteractable>();
+            if (_interactable != null)
             {
-                interactable.Interact(this);
+                if (_interactionPromptUI.IsDisplayer) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+
+                if (Keyboard.current.eKey.wasPressedThisFrame) _interactable.Interact(this);
             }
+        }
+        else
+        {
+            if (_interactable != null) _interactable = null;
+            if (_interactionPromptUI.IsDisplayer) _interactionPromptUI.Close();
         }
 
     }
