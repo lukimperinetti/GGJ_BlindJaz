c4bdc9b046313ef6cc546117f930883df5305f8d
BlindCapt <matthieujulie07@gmail.com>
2024-01-27 18:24:27 +0100

    Fix

--- Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Door.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/Door.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
-    [SerializeField] Transform player;
+    [SerializeField] GameObject player;
+    [SerializeField] private CharacterController _controller;
+
     public string InteractionPrompt => _prompt;
     /*public Vector3 destinationPosition = new Vector3(-10, 0.2f, -158.24f);*/
 
     public bool Interact(Interactor interactor)
     {
-     
         Debug.Log("toto !");
-        player.transform.position = new Vector3(-10f, 0.2f, -158.24f);
+
+        _controller.enabled = false;
+        player.transform.position = new Vector3(-14f, 0.18f, -165.62f);
+        _controller.enabled = true;
+
         return true;
     }
 }
