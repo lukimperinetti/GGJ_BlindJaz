43c85bd4e850d1a52dccd1da1f1bb6e769751608
BlindCapt <matthieujulie07@gmail.com>
2024-01-27 20:29:51 +0100

    start of street performence

---
+++ Assets/CharacterManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public int fatigue = 0;
+    public int reputation = 0;
+    public int money = 0;
+
+}
