14034a8577182f9943bca0150f2b6b2ce12790bc
BlindCapt <matthieujulie07@gmail.com>
2024-01-28 17:52:55 +0100

    Block movement and interact when rest and perform

--- Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/sketch_perfomance.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/InteractionSystem/sketch_perfomance.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using AIPackage;
+using UnityEngine.UI;
 
 public class sketch_perfomance : MonoBehaviour, IInteractable
 {
@@ -10,7 +11,21 @@ public class sketch_perfomance : MonoBehaviour, IInteractable
     [SerializeField] private CharacterManager player;
     [SerializeField] private AIPackage.DayAndNightControl dayAndNightControl;
     [SerializeField] private PopUpMessage popUpMessage;
-    [SerializeField] private Fade fade;
+    [SerializeField] private Image fade;
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+
+    private void Update()
+    {
+        if (fadeIn)
+        {
+            FadeIn();
+        }
+        if (fadeOut)
+        {
+            FadeOut();
+        }
+    }
 
     public bool Interact(Interactor interactor)
     {
@@ -19,12 +34,42 @@ public class sketch_perfomance : MonoBehaviour, IInteractable
             popUpMessage.showMessage("You are too tired to perform", 3);
             return false;
         }
-        fade.FadeIn();
+        if (!fadeIn && !fadeOut)
+        {
+            fadeIn = true;
+            player.GetComponent<CharacterController>().enabled = false;
+        }
+
+        return true;
+    }
+
+    private void FadeIn()
+    {
+        fade.GetComponent<Image>().color = new Color(0, 0, 0, fade.GetComponent<Image>().color.a + 0.01f);
+        if (fade.GetComponent<Image>().color.a >= 1.25f)
+        {
+            fadeOut = true;
+            fadeIn = false;
+            IncrementeStats();
+        }
+    }
+
+    private void FadeOut()
+    {
+        fade.GetComponent<Image>().color = new Color(0, 0, 0, fade.GetComponent<Image>().color.a - 0.01f);
+        if (fade.GetComponent<Image>().color.a <= 0)
+        {
+            fadeOut = false;
+            player.GetComponent<CharacterController>().enabled = true;
+
+        }
+    }
+
+    private void IncrementeStats()
+    {
         player.fatigue += 10;
         player.reputation += 10;
         player.money += 10;
         dayAndNightControl.currentTime += 0.2f;
-
-        return true;
     }
 }
