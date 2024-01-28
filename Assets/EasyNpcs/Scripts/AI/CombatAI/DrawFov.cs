767897f8601eac9d666084e736515383c4ed7c90
lukimperinetti <luk.imperinetti@epitech.eu>
2024-01-28 16:33:16 +0100

    Build GJ
    Escape de CombatScript

--- Assets/EasyNpcs/Scripts/AI/CombatAI/DrawFov.cs
+++ Assets/EasyNpcs/Scripts/AI/CombatAI/DrawFov.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -26,4 +26,4 @@ namespace AIPackage
             }
         }
     }
-}
+}*/
